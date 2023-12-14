bfd974221a6fc1f37c5f7ef6964b05831604667a
Vitor <vitorguilherme286@gmail.com>
2023-12-13 21:45:02 -0300

    first commit

---
+++ DOM/Fokus-master/script.js
@@ -0,0 +1,62 @@
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+
+const img = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll('.app__card-button');
+
+const musicaFocoInput = document.querySelector('#alternar-musica');
+const musica = new Audio('sons/luna-rise-part-one.mp3');
+musica.loop = true;
+
+musicaFocoInput.addEventListener("change", function(){
+    if(musica.paused){
+        musica.play();
+    } else{
+        musica.pause();
+    }
+})
+
+focoBt.addEventListener("click", function () {
+    alterarContexto('foco');
+    focoBt.classList.add('active');
+})
+
+curtoBt.addEventListener("click", function () {
+    alterarContexto('descanso-curto');
+    curtoBt.classList.add('active');
+})
+
+longoBt.addEventListener("click", function () {
+    alterarContexto('descanso-longo');
+    longoBt.classList.add('active');
+})
+
+function alterarContexto(contexto) {
+    botoes.forEach(function(contexto){
+        contexto.classList.remove('active');
+    })
+    html.setAttribute("data-contexto", contexto);
+    img.setAttribute("src", `imagens/${contexto}.png`); 
+    switch (contexto) {
+        case "foco":
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+        break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+            Que tal dar uma respirada? <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+        break;
+        case "descanso-longo":
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        default:
+        break;
+    }
+}
