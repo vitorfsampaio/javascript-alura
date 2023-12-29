5132dfb9a8e5de27edc54ea1b1d270751cab2c40
Vitor <vitorguilherme286@gmail.com>
2023-12-28 23:46:58 -0300

    0

--- LocalStorage/fokus-local-storage-aula-1/script-crud.js
+++ LocalStorage/fokus-local-storage-aula-1/script-crud.js
@@ -5,7 +5,11 @@ const ulTarefas = document.querySelector('.app__section-task-list');
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 const btncDeletar = document.querySelector('.app__form-footer__button--delete');
 
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description');
+
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
+
+let tarefaSelecionada = null;
 
 const deletarFormulario = () => {
     textarea.value = '';
@@ -44,7 +48,6 @@ function criarElementoTarefa(tarefa) {
 
     botao.onclick = () => {
         var novaDescricao = prompt('Qual é o novo nome da tarefa?');
-        console.log('Nova descrição de tarefa:', novaDescricao);
         if (novaDescricao) {
             paragrafo.textContent = novaDescricao;
             tarefa.descricao = novaDescricao;
@@ -59,6 +62,23 @@ function criarElementoTarefa(tarefa) {
     li.append(svg);
     li.append(paragrafo);
     li.append(botao);
+
+    li.onclick = () => {
+        debugger;
+        document.querySelectorAll('.app__section-task-list-item-active')
+            .forEach(elemento => {
+                elemento.classList.remove('app__section-task-list-item-active');
+            });
+        if (tarefaSelecionada == tarefa) {
+            paragrafoDescricaoTarefa.textContent = '';
+            tarefaSelecionada = null;
+            return;
+        }
+        tarefaSelecionada = tarefa;
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+
+        li.classList.add('app__section-task-list-item-active');
+    }
 
     return li;
 }
@@ -84,21 +104,3 @@ tarefas.forEach(tarefa => {
     const elementoTarefa = criarElementoTarefa(tarefa);
     ulTarefas.append(elementoTarefa);
 });
-
-
-/*
-<li class="app__section-task-list-item">
-    <svg>
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
-            <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
-        </svg>
-    </svg>
-    <p class="app__section-task-list-item-description">
-        Estudando localStorage
-    </p>
-    <button class="app_button-edit">
-        <img src="/imagens/edit.png">
-    </button>
-</li>
-*/
