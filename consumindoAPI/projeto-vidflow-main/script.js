8a9ebcc5818f5efc026604037cce6a1cafaeddd7
Vitor <vitorguilherme286@gmail.com>
2024-01-15 19:36:30 -0300

    0

--- consumindoAPI/projeto-vidflow-main/script.js
+++ consumindoAPI/projeto-vidflow-main/script.js
@@ -1,9 +1,14 @@
 const containerVideos = document.querySelector('.videos__container');
 
-const api = fetch("http://localhost:3000/videos")
-    .then(res => res.json())
-    .then((videos) =>
+async function buscarEMostrarVideos() {
+    try {
+        const busca = await fetch("http://localhost:3000/videos")
+        const videos = await busca.json()
+
         videos.forEach((video) => {
+            if (video.categoria == '') {
+                throw new Error('Vídeo não tem categoria!')
+            }
             containerVideos.innerHTML += `
             <li class="videos__item">
                 <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
@@ -15,14 +20,12 @@ const api = fetch("http://localhost:3000/videos")
             </li>
         `
         })
+    } catch(error){
+        containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}`
+    }
+}
 
-    )
-    
-.catch((error)=>{
-    containerVideos.innerHTML = `
-        <p>Houve um erro ao carregar os vídeos: ${error}</p>
-    `
-})
+buscarEMostrarVideos();
 
 
 
