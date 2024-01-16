79031f12b3b3238e82e618e18b879693f49e96f2
Vitor <vitorguilherme286@gmail.com>
2024-01-16 15:03:39 -0300

    last commit

--- consumindoAPI/projeto-vidflow-main/script.js
+++ consumindoAPI/projeto-vidflow-main/script.js
@@ -16,27 +16,53 @@ async function buscarEMostrarVideos() {
                     <img class="img-canal" src="${video.imagem}" alt="Logo do canal">
                     <h3 class="titulo-video">${video.titulo}</h3>
                     <p class="titulo-canal">${video.descricao}</p>
+                    <p class = "categoria" hidden>${video.categoria}</p>
                 </div>
             </li>
         `
         })
-    } catch(error){
+    } catch (error) {
         containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}`
     }
 }
 
 buscarEMostrarVideos();
 
+const barraDePesquisa = document.querySelector('.pesquisar__input');
 
+barraDePesquisa.addEventListener("input", filtrarPesquisa)
 
+function filtrarPesquisa() {
+    const videos = document.querySelectorAll('.videos__item');
+    const valorFiltro = barraDePesquisa.value.toLowerCase();
 
+    videos.forEach((video) => {
+        const titulo = video.querySelector('.titulo-video').textContent.toLowerCase();
 
+        video.style.display = valorFiltro ? titulo.includes(valorFiltro) ? 'block' : 'none' : 'block';
+    });
+}
 
+const botaoCategoria = document.querySelectorAll(".superior__item");
 
+botaoCategoria.forEach((botao) => {
+    let nomeCategoria = botao.getAttribute("name");
+    botao.addEventListener('click', () =>{filtrarPorCategoria(nomeCategoria)});
+})
 
+function filtrarPorCategoria(filtro){
+    const videos = document.querySelectorAll('.videos__item')
+    for(let video of videos){
+        let categoria = video.querySelector('.categoria').textContent.toLowerCase();
+        let valorFiltro = filtro.toLowerCase();
 
-
-
+        if(!categoria.includes(valorFiltro) && valorFiltro != "tudo"){
+            video.style.display = 'none';
+        } else{
+            video.style.display = 'block';
+        }
+    }
+}
 
 
 
