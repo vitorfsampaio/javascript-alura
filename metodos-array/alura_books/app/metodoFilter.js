8ed9b73c9cd684febb94ad7e362fbea1f4b081a5
Vitor <vitorguilherme286@gmail.com>
2024-01-21 12:34:39 -0300

    last commit

--- metodos-array/alura_books/app/metodoFilter.js
+++ metodos-array/alura_books/app/metodoFilter.js
@@ -7,7 +7,9 @@ function filtrarLivros() {
     var livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria)
     exibirOsLivrosNaTela(livrosFiltrados);
     if (categoria == 'disponivel') {
-        exibirValorTotalDosLivros()
+        const valorTotal = calcularValorTotal(livrosFiltrados)
+        console.log(valorTotal);
+        exibirValorTotalDosLivros(valorTotal)
     }
 }
 
@@ -19,10 +21,10 @@ function filtrarPorDisponibilidade() {
     return livros.filter(livro => livro.quantidade > 0)
 }
 
-function exibirValorTotalDosLivros() {
+function exibirValorTotalDosLivros(valorTotal) {
     elementoValorTotal.innerHTML = `
         <div class="livros__disponiveis">
-        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+        <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
         </div>
     `
 }
