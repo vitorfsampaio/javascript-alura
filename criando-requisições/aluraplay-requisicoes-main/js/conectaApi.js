9c0b65b37405ad5ebcd296b7edc724176c2a236b
Vitor <vitorguilherme286@gmail.com>
2024-01-24 23:31:25 -0300

    0

--- criando-requisições/aluraplay-requisicoes-main/js/conectaApi.js
+++ criando-requisições/aluraplay-requisicoes-main/js/conectaApi.js
@@ -1,13 +1,11 @@
 async function listaVideos(){
     const conexao = await fetch('http://localhost:3000/videos')
-    conexaoConvertida = await conexao.json()
-    
+    const conexaoConvertida = await conexao.json()
+
     return conexaoConvertida;
 }
 
-export const conectaApi = {
-    listaVideos
-}
+export const conectaApi = {listaVideos}
 
 
 //json-server --watch db.json
