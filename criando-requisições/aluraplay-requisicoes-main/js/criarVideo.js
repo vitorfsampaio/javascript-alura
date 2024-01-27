906083671ffcaf847a6f01ca20e1c741b24d287a
Vitor <vitorguilherme286@gmail.com>
2024-01-27 18:06:22 -0300

    last commit

--- criando-requisições/aluraplay-requisicoes-main/js/criarVideo.js
+++ criando-requisições/aluraplay-requisicoes-main/js/criarVideo.js
@@ -2,16 +2,20 @@ import { conectaApi } from "./conectaApi.js";
 
 const formulario = document.querySelector("[data-formulario]");
 
-async function criarVideo(evento){
+async function criarVideo(evento) {
     evento.preventDefault();
     const imagem = document.querySelector("[data-imagem]").value;
     const url = document.querySelector("[data-url]").value;
     const titulo = document.querySelector("[data-titulo]").value;
     const descricao = Math.floor(Math.random() * 100).toString();
+    try {
+        await conectaApi.criaVideo(titulo, descricao, url, imagem)
 
-    await conectaApi.criaVideo(titulo, descricao, url, imagem)
+        window.location.href = "../pages/envio-concluido.html"
+    } catch(e){
+        alert(e)
+    }
 
-    window.location.href = "../pages/envio-concluido.html"
 }
 
 formulario.addEventListener('submit', evento => criarVideo(evento))
