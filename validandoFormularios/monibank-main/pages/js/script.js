6a8651ba0c1820e82274b13e26aa5bee65e976f2
Vitor <vitorguilherme286@gmail.com>
2024-01-30 12:15:30 -0300

    first commit

---
+++ validandoFormularios/monibank-main/pages/js/script.js
@@ -0,0 +1,13 @@
+import ehUmCPf from "./valida-cpf.js";
+
+const camposDoFormulario = document.querySelectorAll("[required]")
+
+camposDoFormulario.forEach((campo) => {
+    campo.addEventListener("blur", () => verificaCampo(campo))
+})
+
+function verificaCampo(campo) {
+    if(campo.name == "cpf" && campo.value.length >= 11){
+        ehUmCPf(campo)
+    }
+}
---
+++ validandoFormularios/monibank-main/pages/js/valida-cpf.js
@@ -0,0 +1,4 @@
+export default function ehUmCPf(campo){
+    const cpf = campo.value.replace(/\.|-/g, "");
+    console.log(cpf);
+}
