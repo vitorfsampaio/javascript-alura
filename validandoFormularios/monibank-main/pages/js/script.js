0c32c453c48180392aa159ee60385f07f5736fc6
Vitor <vitorguilherme286@gmail.com>
2024-02-05 22:49:07 -0300

    0

--- validandoFormularios/monibank-main/pages/js/script.js
+++ validandoFormularios/monibank-main/pages/js/script.js
@@ -1,13 +1,19 @@
 import ehUmCPf from "./valida-cpf.js";
+import ehMaiorDeIdade from "./valida-idade.js";
 
 const camposDoFormulario = document.querySelectorAll("[required]")
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo))
+    campo.addEventListener('invalid', evento =>{evento.preventDefault()})
 })
 
 function verificaCampo(campo) {
     if (campo.name == "cpf" && campo.value.length >= 11) {
         ehUmCPf(campo)
+    }
+
+    if (campo.name == "aniversario" && campo.value != "") {
+        ehMaiorDeIdade(campo)
     }
 }
--- validandoFormularios/monibank-main/pages/js/valida-cpf.js
+++ validandoFormularios/monibank-main/pages/js/valida-cpf.js
@@ -1,8 +1,10 @@
 export default function ehUmCPf(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    validaNumerosRepetidos(cpf)
-    validaPrimeiroDigito(cpf)
-    console.log(validaNumerosRepetidos(cpf));
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        console.log('Esse cpf não existe!');
+    } else{
+        console.log('Existe!');
+    }
 }
 
 function validaNumerosRepetidos() {
@@ -22,7 +24,7 @@ function validaNumerosRepetidos() {
     return numerosRepetidos.includes(cpf)
 }
 
-function validaPrimeiroDigito() {
+function validaPrimeiroDigito(cpf) {
     let soma = 0
     let multiplicador = 10
 
@@ -37,5 +39,23 @@ function validaPrimeiroDigito() {
         soma = 0
     }
 
-    return soma!=cpf[9]
+    return soma != cpf[9]
+}
+
+function validaSegundoDigito(cpf) {
+    let soma = 0
+    let multiplicador = 11
+
+    for (let tamanho = 0; tamanho > 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador
+        multiplicador--
+    }
+
+    soma = (soma * 10) % 11;
+
+    if (soma == 10 || soma == 11) {
+        soma = 0
+    }
+
+    return soma != cpf[10]
 }
